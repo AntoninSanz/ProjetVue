16a1f2f318aa38d1a81a5cd00389f238ff7df006
Lucas <lucas.carlier@univ-lille.fr>
2022-04-08 16:21:29 +0200

    composant erreur

---
+++ src/components/Erreur.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Message severity="error" :closable="false">Page inconnu !!!!</Message>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core";
+import Message from "primevue/message";
+
+export default defineComponent
+({
+    components:
+    {
+        Message
+    }
+});
+</script>
